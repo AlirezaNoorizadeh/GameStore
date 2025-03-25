9dfbd1ec97278f4f2bc82af9075a9cf7f12e4011
ARNZ-H <alirezanoorizadeh82@gmail.com>
2025-03-25 19:42:11 +0330

    initial

---
+++ GameStore.Api/DTOs/CreateGameDto.cs
@@ -0,0 +1,7 @@
+namespace GameStore.Api.DTOs;
+
+public record class CreateGameDto(
+    string Name, 
+    string Genre, 
+    decimal Price, 
+    DateOnly ReleaseDate);
---
+++ GameStore.Api/DTOs/UpdateGameDto.cs
@@ -0,0 +1,7 @@
+namespace GameStore.Api.DTOs;
+
+public record class UpdateGameDto(
+    string Name, 
+    string Genre, 
+    decimal Price, 
+    DateOnly ReleaseDate);
